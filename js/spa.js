fbe859515faa353e7d8f54ac897761d57d030b07
Danilo Zekovic <danilozeka93@gmail.com>
2016-03-07 15:49:25 -0500

    just a module

---
+++ js/spa.js
@@ -0,0 +1,14 @@
+/*
+ * spa.js
+ * Danilo Zekovic
+ * OSCON 2016
+*/
+
+var spa = (function () {
+  'use strict';
+  var initModule = function ( $container ) {
+    // Start the shell
+    spa.shell.initModule( $container );
+  };
+  return { initModule: initModule };
+}());
