fdce918337da648e1b72771b65ca28ce5c5e67fb
Danilo Zekovic <danilozeka93@gmail.com>
2016-03-07 18:31:24 -0500

    some changes

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -25,8 +25,6 @@ spa.shell = (function () {
       + '<div class="splash-container">'
       +   '<div class="splash">'
       +     '<h1 class="splash-head">Welcome to Fountain of History!</h1>'
-      +     '<p class="splash-subhead">Place where you can place and search old pictures.</p>'
-      +     '<p><a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a></p>'
       +   '</div>'
       + '</div>'
 
