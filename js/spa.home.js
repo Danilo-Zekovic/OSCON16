4f79384602b284d5221c5db5a8f79692bace8c96
Brian Capouch <brianc@palaver.net>
2016-03-10 05:15:15 -0500

    Wording changes; add putative project name

--- js/spa.home.js
+++ js/spa.home.js
@@ -49,7 +49,7 @@ spa.home = (function (){
         +           'Get Started!!'
         +         '</h3></a>'
         +         '<p>'
-        +           'Learn how to upload, tag, and manage your images using PRODUCT-NAME'
+        +           'Learn how to upload, tag, and manage your images using '
         +         '</p>'
         +       '</div>'
 
