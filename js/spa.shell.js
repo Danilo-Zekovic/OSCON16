c4f970ee82af9f371329d15af1125c3ef190fdf7
Danilo Zekovic <danilozeka93@gmail.com>
2016-03-10 23:58:49 -0500

    something

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -217,6 +217,13 @@ spa.shell = (function () {
     page('/login', login);
     page();
 
+    // event handlers
+    $(".pure-menu-item").click(function(){
+      $( ".pure-menu-item" ).each(function( index ) {
+        $(this).removeClass("pure-menu-selected");
+      });
+      $(this).toggleClass("pure-menu-selected");
+    });
 
 
   }; // End public method initModule
