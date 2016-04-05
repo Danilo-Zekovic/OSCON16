90a64bbd01e0d66bea8f83756a59e987ff74248e
Brian Capouch <brianc@palaver.net>
2016-04-05 06:24:06 -0400

    Marching to a different drummer

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Get our "initial" function to boot up with
 import initModule from './js/spa';
+var React = require('react');
+console.log('Im not afraid now, so much: '+ React);
 
 // Note we're keeping jQuery (for now) as seen by the '$'
 spa.initModule($('#spa'));
