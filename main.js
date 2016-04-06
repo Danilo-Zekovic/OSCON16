762390caba8a2330c45aa1fce65bbd4e51a1044c
Brian Capouch <brianc@palaver.net>
2016-04-06 16:37:00 -0400

    Add comment

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var React = require('react');
 
 // Stage Two: Get our custom "classic SPA" modules
 // (N.B. Dots in identifiers are problematical in ES6)
+// Also note that the shell isn't loaded here; it's loaded inside spa.js
 import spaInitModule from './js/spa';
 import './js/spa.home';
 import './js/spa.upload';
