4a4ce7a4160ee17fa5329a25840da84671956ef9
Brian Capouch <brianc@palaver.net>
2016-04-06 18:23:57 -0400

    Code refactoring

--- js/spa.upload.js
+++ js/spa.upload.js
@@ -12,7 +12,7 @@ spa.upload = (function (){
   var
     configMap = {
       main_html : String()
-        +     '<h2 class="content-head is-center">UPLOAD YOUR IMAGES</h2>'
+        +     '<h2 class="content-head is-center">UPLOAD YOUR images</h2>'
         +     '<div class="pure-g">'
 
         +     '<div class="l-box-lrg pure-u-1 pure-u-md-2-5">'
@@ -67,45 +67,45 @@ spa.upload = (function (){
       $container.hide();
 
   // This example pilfered from "Thinking in React" on the React website
-  var ProductCategoryRow = React.createClass({
+  var ImageCategoryRow = React.createClass({
         render: function() {
           return (<tr><th colSpan="2">{this.props.category}</th></tr>);
         }
       });
 
-  var ProductRow = React.createClass({
+  var ImageRow = React.createClass({
     render: function() {
-      var name = this.props.product.restricted ?
-        this.props.product.name :
+      var name = this.props.image.restricted ?
+        this.props.image.name :
         <span style={{color: 'red'}}>
-          {this.props.product.name}
+          {this.props.image.name}
         </span>;
       return (
         <tr>
           <td>{name}</td>
-          <td>{this.props.product.Circa}</td>
+          <td>{this.props.image.circa}</td>
         </tr>
       );
     }
   });
 
-  var ProductTable = React.createClass({
+  var ImageTable = React.createClass({
     render: function() {
       var rows = [];
       var lastCategory = null;
-      this.props.products.forEach(function(product) {
-        if (product.category !== lastCategory) {
-          rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      this.props.images.forEach(function(image) {
+        if (image.category !== lastCategory) {
+          rows.push(<ImageCategoryRow category={image.category} key={image.category} />);
         }
-        rows.push(<ProductRow product={product} key={product.name} />);
-        lastCategory = product.category;
+        rows.push(<ImageRow image={image} key={image.name} />);
+        lastCategory = image.category;
       });
       return (
         <table>
           <thead>
             <tr>
               <th>Name</th>
-              <th>Circa</th>
+              <th>circa</th>
             </tr>
           </thead>
           <tbody>{rows}</tbody>
@@ -129,28 +129,28 @@ spa.upload = (function (){
     }
   });
 
-  var FilterableProductTable = React.createClass({
+  var FilterableImageTable = React.createClass({
     render: function() {
       return (
         <div>
           <SearchBar />
-          <ProductTable products={this.props.products} />
+          <ImageTable images={this.props.images} />
         </div>
       );
     }
   });
 
 
-  var PRODUCTS = [
-    {category: 'RPPC Postcards', Circa: '1889', restricted: true, name: 'IroquoisBridge.tif'},
-    {category: 'RPPC Postcards', Circa: '1906', restricted: true, name: 'GangwerHotel.tif'},
-    {category: 'RPPC Postcards', Circa: '1914', restricted: false, name: 'HannaWest.png'},
-    {category: 'Family Photos B/W', Circa: '1936', restricted: true, name: 'JAWilliams2.tif'},
-    {category: 'Family Photos B/W', Circa: '1922', restricted: false, name: 'Dawson1922.png'},
-    {category: 'Family Photos B/W', Circa: '1950', restricted: true, name: 'PowlerReunion.tif'}
+  var images = [
+    {category: 'RPPC Postcards', circa: '1889', restricted: true, name: 'IroquoisBridge.tif'},
+    {category: 'RPPC Postcards', circa: '1906', restricted: true, name: 'GangwerHotel.tif'},
+    {category: 'RPPC Postcards', circa: '1914', restricted: false, name: 'HannaWest.png'},
+    {category: 'Family Photos B/W', circa: '1936', restricted: true, name: 'JAWilliams2.tif'},
+    {category: 'Family Photos B/W', circa: '1922', restricted: false, name: 'Dawson1922.png'},
+    {category: 'Family Photos B/W', circa: '1950', restricted: true, name: 'PowlerReunion.tif'}
   ];
   ReactDOM.render(
-    <FilterableProductTable products={PRODUCTS} />,
+    <FilterableImageTable images={images} />,
     document.getElementById('upload-view')
   );
       // $container.html( configMap.main_html ).show();
