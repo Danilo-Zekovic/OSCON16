54d59e775cab607a5157db48191588340cd40862
Brian Capouch <brianc@palaver.net>
2016-04-07 02:47:10 -0400

    Use multer package to fulfill upload

--- server.js
+++ server.js
@@ -5,14 +5,15 @@
  */
 
 'use strict';
-//
+
 var
-  setWatch,
   http = require('http'),
   express = require('express'),
+  multer = require('multer'),
   bodyParser = require('body-parser'),
-  methodOverride = require('method-override'),
-  morgan = require('morgan'),
+  MulterImpl  = require('./js/multerImpl'),
+  // methodOverride = require('method-override'),
+  // morgan = require('morgan'),
 
   app = express(),
   router = express.Router(),
@@ -20,12 +21,12 @@ var
   server = http.createServer( app );
 //------------------------------------------------------
 
-
     // -- Configure server behavior
     app.use( express.static( __dirname + '/' ) );
     app.use( bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
-    app.use(methodOverride());
+    app.use(new MulterImpl({}).init());
+    // app.use(methodOverride());
     // Turn on verbose logging when needed by uncommenting line below
     // app.use(morgan('combined'));
     routes.configRoutes( router, server);
